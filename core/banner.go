f40b877814caaa499bc752121c69030375dfdc7e
Ohpe <704560+ohpe@users.noreply.github.com>
2020-06-24 12:52:47 +0200

    Releasing v0.1.4

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "Muraena"
-	Version = "0.1.3"
+	Version = "0.1.4"
 	Author  = "Muraena Team"
 	Website = "https://phishing.click/"
 	Banner  = `           ___   ____
