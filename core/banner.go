a0e772389c4f0d4b031eb4bc2ee14be6e04e7961
Giuseppe Trotta <704560+ohpe@users.noreply.github.com>
2021-02-22 21:54:11 +0100

    Releasing v1.2

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "Muraena"
-	Version = "1.1.0"
+	Version = "1.2"
 	Author  = "Muraena Team"
 	Website = "https://phishing.click/"
 	Banner  = `           ___   ____
