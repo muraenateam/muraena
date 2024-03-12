13bd44d68e3eb8a6b88db19251acda4329744919
Ohpe <704560+ohpe@users.noreply.github.com>
2024-03-12 13:59:39 +0100

    Releasing v1.21.1 :tada:

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "Muraena"
-	Version = "1.21"
+	Version = "1.21.1"
 	Author  = "Muraena Team"
 	Website = "https://phishing.click/"
 	Banner  = `           ___   ____
